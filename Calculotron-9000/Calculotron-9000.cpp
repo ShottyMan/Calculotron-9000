aedc42b4e535130abeddf3394ff02098c9816d82
ShottyMan <Jyssauco1@gmail.com>
2021-03-05 16:47:19 -0500

    Add project files.

---
+++ Calculotron-9000/Calculotron-9000.cpp
@@ -0,0 +1,12 @@
+﻿// Calculotron-9000.cpp : Defines the entry point for the application.
+//
+
+#include "Calculotron-9000.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
