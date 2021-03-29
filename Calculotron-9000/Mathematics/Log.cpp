03f82d0a7da94cac7af815a9b82572c4f38350e1
ShottyMan <Jyssauco1@gmail.com>
2021-03-28 20:06:36 -0400

    Chaning around some files

--- Calculotron-9000/Mathematics/Log.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../Calculotron-9000/MainHeader.hpp"
-#include <cmath>
-
-void findign(double Tn, double a, double r, double n)
-{
-  n = (log((Tn/a))/(log(r))) + 1;
-}
--- Calculotron-9000/Mathematics/MathHeader.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "../MainHeader"
-
-double arithsum(double, double, double);
-void xinter(double, double, double, double&, double&);
-void findign(double Tn, double a, double r, double n);
